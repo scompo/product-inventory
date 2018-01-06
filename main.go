c018853d706130c26faefa53c32f712446c284db
Mauro Scomparin <scompo@gmail.com>
2018-01-06 18:38:51 +0100

    Some tests.

--- main.go
+++ main.go
@@ -2,10 +2,47 @@ package main
 
 import "fmt"
 
+// Product in the inventory.
 type Product struct {
 	Id       string
 	Price    float64
 	Quantity int64
+}
+
+// An inventory of products.
+type Inventory struct {
+	data map[string]*Product
+}
+
+// Creates an empty Inventory
+func EmptyInventory() Inventory {
+	return Inventory{data: map[string]*Product{}}
+}
+
+// Creates a new Product
+func NewProduct(id string, price float64, qt int64) *Product {
+	p := new(Product)
+	p.Id = id
+	p.Price = price
+	p.Quantity = qt
+	return p
+}
+
+// Returns true if the id of the product is present, false otherwise.
+func (i Inventory) Present(id string) bool {
+	_, present := i.data[id]
+	return present
+}
+
+// Adds or updates the status of a Product.
+func (i *Inventory) Add(p *Product) *Product {
+	if i.Present(p.Id) {
+		i.data[p.Id].Price = p.Price
+		i.data[p.Id].Quantity = i.data[p.Id].Quantity + p.Quantity
+	} else {
+		i.data[p.Id] = p
+	}
+	return i.data[p.Id]
 }
 
 func main() {
--- main_test.go
+++ main_test.go
@@ -1,7 +1,59 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestSomething(t *testing.T) {
-	// should run?
+func TestPresentWithNewElement(t *testing.T) {
+	i := EmptyInventory()
+	present := i.Present("not present")
+	if present {
+		t.Errorf("present and should not!")
+	}
+}
+
+func TestPresentWithExistingElement(t *testing.T) {
+	i := EmptyInventory()
+	p := NewProduct("present", 10.00, 20)
+	i.Add(p)
+	present := i.Present("present")
+	if !present {
+		t.Errorf("not present and should be!")
+	}
+}
+
+func TestAddNewElement(t *testing.T) {
+	i := EmptyInventory()
+	p := NewProduct("present", 10.00, 20)
+	added := i.Add(p)
+	if added.Id != "present" {
+		t.Errorf("id not correct")
+	}
+	if added.Price != 10.00 {
+		t.Errorf("price not correct")
+	}
+	if added.Quantity != 20 {
+		t.Errorf("quantity not correct")
+	}
+}
+
+func TestAddWithExistingElement(t *testing.T) {
+	i := EmptyInventory()
+	p := NewProduct("present", 10.00, 20)
+	p2 := NewProduct("present", 20.20, 10)
+	i.Add(p)
+	added := i.Add(p2)
+
+	fmt.Println(added)
+
+	if added.Id != "present" {
+		t.Errorf("id not correct")
+	}
+	if added.Price != 20.20 {
+		t.Errorf("price not correct")
+	}
+	if added.Quantity != 30 {
+		t.Errorf("quantity not correct")
+	}
 }
