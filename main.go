afd3bb1ab265dd6eaa170ac9a54a0160591608ad
Mauro Scomparin <scompo@gmail.com>
2018-01-07 02:43:47 +0100

    Implemented basic ui.

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
+	"os"
+	"strconv"
+)
+
+const (
+	QUIT   = "quit"
+	VALUE  = "value"
+	STATUS = "status"
+	INSERT = "insert"
+	UPDATE = "update"
 )
 
 // Product in the inventory.
@@ -12,6 +23,7 @@ type Product struct {
 	Quantity int64
 }
 
+// Returns the value of a Product.
 func (p Product) Value() float64 {
 	return p.Price * float64(p.Quantity)
 }
@@ -79,6 +91,110 @@ func (i *Inventory) Update(p *Product) (*Product, error) {
 	return i.data[p.Id], nil
 }
 
+func printStatus(p *Product) {
+	fmt.Printf("Product id: %s\n", p.Id)
+	fmt.Printf("Product price: %f\n", p.Price)
+	fmt.Printf("Product quantity: %d\n", p.Quantity)
+	fmt.Printf("Product value: %f\n", p.Value())
+}
+
+func printMenu() {
+	fmt.Printf("Available operations:\n")
+	fmt.Printf("%s\tExits\n", QUIT)
+	fmt.Printf("%s\tPrints the inventory current value\n", VALUE)
+	fmt.Printf("%s\tReturns the status of a product\n", STATUS)
+	fmt.Printf("%s\tInserts a new product\n", INSERT)
+	fmt.Printf("%s\tUpdates an existing product\n", UPDATE)
+}
+
+func printHeader() {
+	fmt.Println("=================")
+	fmt.Println("Product Inventory")
+	fmt.Println("=================")
+}
+
+func printValue(i Inventory) {
+	inventoryValue := i.Value()
+	fmt.Printf("Inventory value: %.2f€\n", inventoryValue)
+}
+
+func readProductId(rc *bufio.Scanner) string {
+	fmt.Printf("Insert product id: ")
+	rc.Scan()
+	return rc.Text()
+}
+
+func readProduct(rc *bufio.Scanner, id string) *Product {
+	var temp string
+
+	fmt.Printf("Insert Product price: ")
+	rc.Scan()
+	temp = rc.Text()
+	price, _ := strconv.ParseFloat(temp, 64)
+
+	fmt.Printf("Insert Product quantity: ")
+	rc.Scan()
+	temp = rc.Text()
+	qt, _ := strconv.ParseInt(temp, 10, 64)
+
+	return NewProduct(id, price, qt)
+}
+
 func main() {
-	fmt.Println("hello!")
+	i := EmptyInventory()
+	rc := bufio.NewScanner(os.Stdin)
+
+	var option string
+
+	for option != QUIT {
+
+		printHeader()
+		printMenu()
+		fmt.Printf("Select operation: ")
+
+		rc.Scan()
+		option = rc.Text()
+
+		if option == VALUE {
+			printValue(i)
+		} else if option == STATUS {
+			id := readProductId(rc)
+			st, err := i.Status(id)
+			if err == nil {
+				printStatus(st)
+			} else {
+				fmt.Printf("%s\n", err.Error())
+			}
+		} else if option == INSERT {
+			id := readProductId(rc)
+			if i.Present(id) {
+				fmt.Printf("Product id already present: %s\n", id)
+			} else {
+				p := readProduct(rc, id)
+				p2, err := i.Add(p)
+				if err != nil {
+					fmt.Printf("%s\n", err.Error())
+				} else {
+					printStatus(p2)
+				}
+			}
+		} else if option == UPDATE {
+			id := readProductId(rc)
+			if !i.Present(id) {
+				fmt.Printf("Product id already present: %s\n", id)
+			} else {
+				p := readProduct(rc, id)
+				p2, err := i.Update(p)
+				if err != nil {
+					fmt.Printf("%s\n", err.Error())
+				} else {
+					printStatus(p2)
+				}
+			}
+		} else if option == QUIT {
+			fmt.Println("Exiting.")
+		} else {
+			fmt.Println("Operation not recognized.")
+		}
+	}
 }
