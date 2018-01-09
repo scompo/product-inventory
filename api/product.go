164c360ce6e64fe7560306ac9c1e4c843ccde308
Mauro Scomparin <scompo@gmail.com>
2018-01-09 01:26:07 +0100

    Created packages for api and cmd

--- api/product.go
+++ api/product.go
@@ -1,4 +1,4 @@
-package main
+package api
 
 // Product in the inventory.
 type Product struct {
--- cmd/inventory-cmd/main.go
+++ cmd/inventory-cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	. "github.com/scompo/product-inventory/api"
 )
 
 const (
